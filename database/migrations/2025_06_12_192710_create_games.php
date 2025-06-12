833860492a4134b91a2d3ff9f542be5a74dc29a6
LunaBrisa <danielalunacast0108@gmail.com>
2025-06-12 14:05:31 -0600

    migraciones

---
+++ database/migrations/2025_06_12_192710_create_games.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('player1_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('player2_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('winner_id')->nullable()->constrained('users')->onDelete('set null');
+            $table->timestamp('started_at')->useCurrent();
+            $table->timestamp('ended_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+};
